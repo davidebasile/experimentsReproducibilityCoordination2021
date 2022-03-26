605df8f553ce370f85ef47d7f963088b93f7df19
Davide Basile <davide.basile@isti.cnr.it>
2022-03-27 00:20:23 +0100

    updating example of png converter to the last version of CATLib
    Signed-off-by: Davide Basile <davide.basile@isti.cnr.it>

---
+++ src/test/java/io/github/contractautomata/expRepScicoSI/AppTest.java
@@ -0,0 +1,17 @@
+package io.github.contractautomata.expRepScicoSI;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest  
+{
+ 
+    @Test
+    public void testExecuteExperiments() throws Exception
+    {
+    	App.main(null);
+    	assert(true);
+    }
+}
